6bc50565332d7be8730eafb88886b41f50c1a7f4
uriel <urielg@cryptouno.app>
2023-01-17 16:59:46 -0300

    Add first screen kt

--- app/src/main/java/com/example/scorelivepoc/InitFragment.kt
+++ app/src/main/java/com/example/scorelivepoc/InitFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.scorelivepoc.databinding.FragmentInitBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,6 +22,14 @@ class InitFragment : Fragment() {
     ): View {
         binding = FragmentInitBinding.inflate(inflater, container, false)
 
+        setListeners()
+
         return binding.root
+    }
+
+    private fun setListeners() {
+        binding.ingresarButton.setOnClickListener {
+            findNavController().navigate(R.id.initFragment_to_scoreFragment)
+        }
     }
 }
