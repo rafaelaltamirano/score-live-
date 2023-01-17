281a665a6bed27f4a70bb3b3eb64dcccded33673
rafaelaltamirano <rafaelaltamirano_@outlook.com>
2023-01-17 19:57:28 -0300

    add recomendation screen

---
+++ app/src/main/java/com/example/scorelivepoc/RecommendationsFragment.kt
@@ -0,0 +1,46 @@
+package com.example.scorelivepoc
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import com.example.scorelivepoc.databinding.FragmentRecommendationsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RecommendationsFragment : Fragment() {
+
+    private lateinit var binding: FragmentRecommendationsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRecommendationsBinding.inflate(inflater, container, false)
+
+
+        fillList()
+
+        return binding.root
+    }
+
+    private fun fillList() {
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf(
+            "1 - Paga tu cuota por adelantado ",
+            "2 - Paga tu cuota por adelantado ",
+            "3 - Paga tu cuota por adelantado ",
+        )
+
+        // access the listView from xml file
+        val mListView = binding.recommendationsListView
+        arrayAdapter = ArrayAdapter(requireContext(),
+            android.R.layout.simple_list_item_1, users)
+        mListView.adapter = arrayAdapter
+    }
+
+
+}
